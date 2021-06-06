9b221fc503323bdbcc0a358722ddbe4ec5a9b8b3
Oliver Moreno <olivermoreno@knights.ucf.edu>
2021-06-06 18:05:30 -0400

    Initial solution to problem 22

--- src/main/java/ex22/App.java
+++ src/main/java/ex22/App.java
@@ -28,6 +28,35 @@ Modify the program so that it asks for an unlimited number of numbers.
  */
 public class App {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        String firstNum = in.nextLine();
+        System.out.print("Enter the second number: ");
+        String secondNum = in.nextLine();
+        System.out.print("Enter the third number: ");
+        String thirdNum = in.nextLine();
+
+        // converting strings to integers
+        int x = Integer.parseInt(firstNum);
+        int y = Integer.parseInt(secondNum);
+        int z = Integer.parseInt(thirdNum);
+
+        // if any of the numbers are the same, exit the program
+        if (x == y || x == z || y == z) {
+            System.exit(0);
+        }
+        // x is the largest number
+        if (x > y && x > z) {
+            System.out.printf("The largest number is %d.\n", x);
+        }
+        // y is the largest number
+        if (y > x && y > z) {
+            System.out.printf("The largest number is %d.\n", y);
+        }
+        // z is the largest number
+        if (z > x && z > y) {
+            System.out.printf("The largest number is %d.\n", z);
+        }
+
     }
 }
